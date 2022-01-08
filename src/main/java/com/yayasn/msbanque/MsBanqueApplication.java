7580af8861bce3a283db8525bd40ce0ba366096a
yayaveli <yabadji2010@gmail.com>
2022-01-08 03:33:52 +0000

    Project Initialisation

---
+++ src/main/java/com/yayasn/msbanque/MsBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.yayasn.msbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBanqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yayasn/msbanque/MsBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yayasn.msbanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
